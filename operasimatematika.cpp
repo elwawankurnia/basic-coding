645d21e7295bcc72a9d15d9fa258489aeed01c98
elwawankurnia <elwawankurnia@gmail.com>
2021-06-04 19:22:24 +0800

    Add files via upload

---
+++ operasimatematika.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int num1;
+  int num2;
+  
+  cout<<"masukkan nilai num1: ";
+  cin>>num1;
+  
+  cout<<"masukkan nilai num2 : ";
+  cin>>num2;
+  cout<<"berikut adalah operasi bilangan tersebut"<<endl;
+  
+  cout<<"num1 + num2: "<<(num1 + num2)<<endl;
+  cout<<"num1 - num2: "<<(num1 - num2)<<endl;
+  cout<<"num1 * num2: "<<(num1 * num2)<<endl;
+  cout<<"num1 / num2: "<<(num1 / num2)<<endl;
+  cout<<"num1 % num2: "<<(num1 % num2)<<endl;
+  return 0;
+}
